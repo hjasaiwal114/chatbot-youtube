9571f21aba091b236cf16418c8b69ba6f4636c94
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-11-02 15:55:09 +0530

    chat bot throwing html

--- src/context/messages.tsx
+++ src/context/messages.tsx
@@ -1,70 +1,67 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 import { nanoid } from 'nanoid'
-import { Message } from "@/lib/validators/message";
+import { Message } from '@/lib/validators/message'
 
 const defaultValue = [
-    {
-        id: nanoid(),
-        text: 'Hello, how can I help you?',
-        isSecureContext: false,
-    },
+  {
+    id: nanoid(),
+    text: 'Hello, how can I help you?',
+    isUserMessage: false,
+  },
 ]
-export const MessageContext = createContext<{
-    messages: Message[]
-    isMessageUpdating: boolean
-    addMessage: (message: Message) => void
-    removeMessage: (id: string) => void
-    updateMessage: (id: string, uodateFn: (prevText: string) => string) => void
-    setIsMessageupdating: (isUpdating: boolean) => void
+export const MessagesContext = createContext<{
+  messages: Message[]
+  isMessageUpdating: boolean
+  addMessage: (message: Message) => void
+  removeMessage: (id: string) => void
+  updateMessage: (id: string, updateFn: (prevText: string) => string) => void
+  setIsMessageUpdating: (isUpdating: boolean) => void
 }>({
-    messages: [],
-    isMessageUpdating: false,
-    addMessage: () => {},
-    removeMessage: () => {},
-    updateMessage: () => {},
-    setIsMessageupdating: () => {},
+  messages: [],
+  isMessageUpdating: false,
+  addMessage: () => {},
+  removeMessage: () => {},
+  updateMessage: () => {},
+  setIsMessageUpdating: () => {},
 })
 
-export function MessageProvider({ children }: {children: React.ReactNode }) {
-    const [message, setMessage] = useState(defaultValue)
-    const [isMessageUpdating, setIsMessageupdating] = useState<boolean>(false)
+export function MessagesProvider({ children }: { children: React.ReactNode }) {
+  const [messages, setMessages] = useState(defaultValue)
+  const [isMessageUpdating, setIsMessageUpdating] = useState<boolean>(false)
 
-    const addMessage = (message: Message) => {
-        setMessage((prev) => [...prev, message])
-    }
+  const addMessage = (message: Message) => {
+    setMessages((prev) => [...prev, message])
+  }
 
-    const removeMessage = (id: string) => {
-        setMessage((prev) => prev.filter((message) => message.id !== id))
-    }
+  const removeMessage = (id: string) => {
+    setMessages((prev) => prev.filter((message) => message.id !== id))
+  }
 
-    const updateMessage =(
-        id: string,
-        updateFn: (prevText: string) => string
-    ) => {
-        setMessage((prev) => 
-          prev.map((message) => {
-            if (message.id === id) {
-                return { ...message, text: updateFn(message.text) }
-            }
-            return message
-          })
-        )
-    }
-    
+  const updateMessage = (
+    id: string,
+    updateFn: (prevText: string) => string
+  ) => {
+    setMessages((prev) =>
+      prev.map((message) => {
+        if (message.id === id) {
+          return { ...message, text: updateFn(message.text) }
+        }
+        return message
+      })
+    )
+  }
 
-
-return (
-    <MessageContext.Provider
+  return (
+    <MessagesContext.Provider
       value={{
         messages,
         isMessageUpdating,
         addMessage,
         removeMessage,
         updateMessage,
-        setIsMessageupdating,
+        setIsMessageUpdating,
       }}>
-        {children}
-      </MessageContext.Provider>
-)
-
+      {children}
+    </MessagesContext.Provider>
+  )
 }
--- src/lib/openai-stream.ts
+++ src/lib/openai-stream.ts
@@ -1,72 +1,80 @@
-import { ParsedEvent, ReconnectInterval} from "eventsource-parser"
-
-export type ChatGptAgent = "user" | "system"
-
-export interface ChatGptMessage {
-    role: ChatGptAgent
-    content: string
-}
-
-export interface OpenAIStreamPayload  {
-    model: 'gpt-3.5-turbo',
-    message: ChatGptMessage[],
-    temperature: 0.4,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penality: 0,
-    max_token: 150,
-    stream: true,
-    n: 1,
-}
-
-export async function OpenAIStream(payload: OpenAIStreamPayload) {
-    const  encoder = next TextEncoder()
-    const decoder = next TextDecoder()
-
-    let counter = 0
-
-    const res = await fetch('http://api.openai.com/vi/chat/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify(payload),
-    })
-
+import {
+    createParser,
+    ParsedEvent,
+    ReconnectInterval,
+  } from "eventsource-parser";
+  
+  export type ChatGPTAgent = "user" | "system";
+  
+  export interface ChatGPTMessage {
+    role: ChatGPTAgent;
+    content: string;
+  }
+  
+  export interface OpenAIStreamPayload {
+    model: string;
+    messages: ChatGPTMessage[];
+    temperature: number;
+    top_p: number;
+    frequency_penalty: number;
+    presence_penalty: number;
+    max_tokens: number;
+    stream: boolean;
+    n: number;
+  }
+  
+  export async function OpenAIStream(payload: OpenAIStreamPayload) {
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+  
+    let counter = 0;
+  
+    const res = await fetch("https://api.openai.com/v1/chat/completions", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
+  
     const stream = new ReadableStream({
-         async start(controller) {
-            function onParse(event: ParsedEvent | ReconnectInterval) {
-                if (event.type === 'event') {
-                    const data = event.data
-                    if(data === '[DONE]') {
-                        controller.close()
-                        return
-                        
-                    }
-                    try {
-                        const json = JSON.parse(data)
-                        console.log("json", json)
-                        const text = json.choce[0].data?.content || ''
-
-                        if(counter > 2 && (text.match(/\n/)|| []).length) {
-                            return 
-                        }
-
-                        const queue = encoder.encode(text)
-                        controller.enqueue(queue)
-
-                    } catch (error) {
-                        controller.error(error)
-                    }
-                }
+      async start(controller) {
+        // callback
+        function onParse(event: ParsedEvent | ReconnectInterval) {
+          if (event.type === "event") {
+            const data = event.data;
+            // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+            if (data === "[DONE]") {
+              controller.close();
+              return;
             }
-
-            const parser = createParser(onParse)
-
-            for await (const chunk of res.body as any) {
-                parser.feed(decoder.decode(chunk))
+            try {
+              const json = JSON.parse(data);
+              const text = json.choices[0].delta?.content || "";
+              if (counter < 2 && (text.match(/\n/) || []).length) {
+                // this is a prefix character (i.e., "\n\n"), do nothing
+                return;
+              }
+              const queue = encoder.encode(text);
+              controller.enqueue(queue);
+              counter++;
+            } catch (e) {
+              // maybe parse error
+              controller.error(e);
             }
-         }
-    })
-}
+          }
+        }
+  
+        // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+        // this ensures we properly read chunks and invoke an event for each SSE event stream
+        const parser = createParser(onParse);
+        // https://web.dev/streams/#asynchronous-iteration
+        for await (const chunk of res.body as any) {
+          parser.feed(decoder.decode(chunk));
+        }
+      },
+    });
+  
+    return stream;
+  }
